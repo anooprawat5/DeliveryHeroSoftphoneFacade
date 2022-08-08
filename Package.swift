d9111f3724f9aaddced3f35df8c57633d5b1c3b2
Stanislav Kutil <skutil@acrobits.cz>
2022-08-08 13:44:59 +0200

    Package updated to

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "SoftphoneFacade",
-                      url: "https://dist.acrobits.cz/deliveryhero-softphonefacade/SoftphoneFacade.xcframework-1806192nCOMMIT=05b6971dbb629ef96be4ef0c54528eb173971e01n.zip",
-                      checksum: "3a52e8e21e1f9320ed24626d10488bf3e96d0470d3a80c123642c24896d959a5"
+                      url: "https://dist.acrobits.cz/deliveryhero-softphonefacade/SoftphoneFacade.xcframework-1806194.zip",
+                      checksum: "28e439b3881f85815453afb4faea2b7889b8c9b8664988c4dfe27cf5eb39ed67"
                      ),
 
     ]
