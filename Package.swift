b2d3dd50b653c8f62cd53d88ed9191ce2c13dbc3
Stanislav Kutil <skutil@acrobits.cz>
2022-08-10 12:07:27 +0200

    Package updated to

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,8 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "SoftphoneFacade",
-                      url: "https://dist.acrobits.cz/deliveryhero-softphonefacade/SoftphoneFacade.xcframework-1806868.zip",
-                      checksum: "d26f1f2725f90c977446b347c5dcf9be12a2693a27c76afe47d6d13cfca3d235"
+                      url: "https://dist.acrobits.cz/deliveryhero-softphonefacade/SoftphoneFacade.xcframework-1807122.zip",
+                      checksum: "f331bbab2a6d55eeb4e8029bece95bba1bcaf1132d463223e7a71a5a04ed5bc8"
                      ),
 
     ]
