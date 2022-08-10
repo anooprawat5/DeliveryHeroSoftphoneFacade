abb068d597fce16cafefb0a3babdb8957bfc6e01
Stanislav Kutil <skutil@acrobits.cz>
2022-08-10 17:44:32 +0200

    Package updated to

--- Package.swift
+++ Package.swift
@@ -14,13 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/acrobits/SoftphoneSwiftPackage-saas-local", "1.0.180710"..<"1.0.180710")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "SoftphoneFacade",
                       url: "https://dist.acrobits.cz/deliveryhero-softphonefacade/SoftphoneFacade.xcframework-1807217.zip",
-                      checksum: "b9c0f96d56d8385eef1aca1d432110b495da1317c2ed4bdae3097b709f3756d3"
+                      checksum: "a86f01fd4569cb3c187d4a79d0ce533a6be3cd752f9a9bd44aba3642026fcd22"
                      ),
 
     ]
