38d6e9b7bc387529568b5b66bccfa8cf806525bf
Anoop Rawat <112691783+anooprawat5@users.noreply.github.com>
2022-10-24 10:25:45 +0200

    Update Package

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "SoftphoneSdk", url: "https://github.com/acrobits/SoftphoneSwiftPackage-saas-local", .exact("1.0.182155"))
+        .package(name: "SoftphoneSwiftPackage-saas-local", url: "https://github.com/acrobits/SoftphoneSwiftPackage-saas-local", .exact("1.0.182155"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "SoftphoneFacadeTargets",
                dependencies: [
-                              .target(name: "SoftphoneFacadeLib"),
+                              .target(name: "SoftphoneSwiftPackage-saas-local"),
                               .product(name: "SoftphoneSwiftPackage-saas-local", package: "SoftphoneSdk")
                               ],
                 linkerSettings: [
